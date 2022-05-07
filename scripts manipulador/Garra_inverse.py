26f71bfc6b37aa3036a55f5464558e1e113146e2
pgalindo310 <73370987+pgalindo310@users.noreply.github.com>
2022-05-07 01:12:54 -0500

    Update Garra_inverse.py

--- scripts manipulador/Garra_inverse.py
+++ scripts manipulador/Garra_inverse.py
@@ -4,39 +4,46 @@ from geometry_msgs.msg import Twist
 import rospy
 import numpy as np
 
-# Consigue las posiciones
-def callback(posicion):
-    global x, y, z
-    x = posicion.x
-    y = posicion.y
-    z = posicion.z
+# # Consigue las posiciones
+# def callback(posicion):
+#     global x, y, z
+#     x = posicion.x
+#     y = posicion.y
+#     z = posicion.z
     
 #Se subcribre al topic para obtener la posicion goal
-def listener():
-    # Se inicia el nodo
-    rospy.init_node('robot_manipulador_planner', anonymous=True)
-    # Se suscribe al topic robot_manipulator_goal para recibir la posición
-    rospy.Subscriber('robot_manipulator_goal', Vector3, callback)
-    rospy.spin()
+# def listener():
+#     # Se inicia el nodo
+
+#     # Se suscribe al topic robot_manipulator_goal para recibir la posición
+#     rospy.Subscriber('robot_manipulator_goal', Vector3, callback)
+#     rospy.spin()
 
 #Publica los angulos para llegar a la posicion
 def talker():
     #Se publica los angulos en el topic
     global x,y,z
-    theta1 = np.arctan(y/x) #Base phi
-    theta2 = np.arctan(z/(np.root(x**2+y**2+z**2)))  #Hombro
+    x = float(input("Ingrese la posicion en x "))
+    y = float(input("Ingrese la posicion en y "))
+    z = float(input("Ingrese la posicion en z "))  # La unica que sirve
+    theta1 = int(np.degrees(np.arctan(y/x) ))#Base phi
+    theta2 = int(np.degrees(np.arctan(z/(np.sqrt(x**2+y**2+z**2))) )) #Hombro
+    rospy.init_node('robot_manipulador_planner', anonymous=True)
     pub = rospy.Publisher('robot_goal', Twist, queue_size = 10)
     rate = rospy.Rate(10)
+
+
+
     while not rospy.is_shutdown():
-        tw = Twist()
-        tw.linear.x = theta2
-        tw.linear.y = theta1
-        pub.publish(tw)
-        rate.sleep()
+            tw = Twist()
+            tw.linear.x = theta2
+            tw.linear.y = theta1
+            pub.publish(tw)
+            rate.sleep()
 
 if __name__ == "__main__":
     try:
-        listener()
+        # listener()
         talker()
     except rospy.ROSInterruptException:
         pass
